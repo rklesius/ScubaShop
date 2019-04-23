f66520d6152d522b3f0c0bb46d7a939716a249ea
Rachel Klesius <rachelklesius@gmail.com>
2019-04-23 12:04:17 -0400

    Program running simplified

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 <h1>Scuba Shop</h1>
 <p>A Scuba shop simulator for Database Design at the University of South Florida</p>
 
-<h2>How to run</h2>
+<h2>Installing the right gems with Ruby</h2>
 <p>sqlite3 needs some help with working with ruby in windows. To get it to run, use the following commands: </p>
 <ul>
     <li>gem uninstall sqlite3 --all</li>
@@ -9,3 +9,9 @@
     <li>gem inst sqlite3 --platform ruby</li>
 </ul>
 <p>Other than that, the only other gem you need to install is Sinatra, using gem install sinatra</p>
+
+<h2>Running the program</h2>
+<p>To run the program, run the following files in this order:</p>
+<p><em>scubashop.rb</em> will initialize the data in the database, both creating the table and adding some data.  </p>
+<p><em>main.rb</em> will then start the webpage on http://localhost:4567/, this webpage will not work unless scubashop.rb is run first</p>
+
--- scubashop.rb
+++ scubashop.rb
@@ -1,14 +1,17 @@
-#scubashop.rb controls the flow of the whole project and queries the database
 
-require 'sinatra'
 require 'sqlite3'
 
-#inintalize the database
+db = SQLite3::Database.open('../scubashop.db')
 
-db = SQLite3::Database.new('scubashop.db')
+begin
+  db.execute("DROP TABLE divers")
+  db.execute("DROP Table dive_trips")
+  db.execute("DROP TABLE employees")
+rescue SQLite3::Exception => e
+  puts "Running table creation...."
+end
 
-#create table in database
-diver_rows = db.execute <<-SQL
+db.execute <<SQL
   create table divers(
       did int,
       email varchar[50],
@@ -16,12 +19,61 @@ diver_rows = db.execute <<-SQL
       dname varchar[50],
       cert_lvl int, 
       dtid int
-  );
+  )
 SQL
 
+db.execute <<SQL
+create table employees(
+ EID int,
+ E_name varchar[50],
+ Salary int,
+ Title varchar[50],
+ Yrs_worked int,
+ Email varchar[50]
+);
+SQL
 
-get '/divers' do
-  erb :divers
-end
+db.execute <<SQL
+create table dive_trips(
+  DTID int,
+  Location varchar[50],
+  Max_depth int,
+  Cert_level_req int,
+  EID int,
+  Cost_per_diver float 
+);
+SQL
+
+puts "Table creation successful"
+
+#inintialize some rows
+db.execute("INSERT INTO divers(did, email, password, dname, cert_lvl, dtid) VALUES(?, ?, ?, ?, ?, ?)",
+           [1, 'rachelklesius@gmail.com', '1234', 'Rachel Klesius', 2, 0 ])
+db.execute("INSERT INTO divers(did, email, password, dname, cert_lvl, dtid) VALUES(?, ?, ?, ?, ?, ?)",
+           [2, 'edduffy@gmail.com', 'password1', 'Edward Duffy', 3, 0 ])
+db.execute("INSERT INTO divers(did, email, password, dname, cert_lvl, dtid) VALUES(?, ?, ?, ?, ?, ?)",
+           [3, 'willfink@gmail.com', 'cat1234', 'William Fink', 5, 1 ])
+db.execute("INSERT INTO divers(did, email, password, dname, cert_lvl, dtid) VALUES(?, ?, ?, ?, ?, ?)",
+           [4, 'samreese@gmail.com', 'sealions', 'Sam Reese', 4, 2 ])
+
+db.execute("INSERT INTO employees(EID, E_name, Salary, Title, Yrs_worked, Email) VALUES(?, ?, ?, ?, ?, ?)",
+           [3, 'John', 15000, 'Sir', 5, 'johnsmith@gmail.com' ])
+db.execute("INSERT INTO employees(EID, E_name, Salary, Title, Yrs_worked, Email) VALUES(?, ?, ?, ?, ?, ?)",
+           [1, 'Michael', 15000, 'Mid', 3, 'mikedavis@gmail.com' ])
+db.execute("INSERT INTO employees(EID, E_name, Salary, Title, Yrs_worked, Email) VALUES(?, ?, ?, ?, ?, ?)",
+           [2, 'Tyler', 20000, 'Lead', 7, 'raggle1221@gmail.com' ])
+db.execute("INSERT INTO employees(EID, E_name, Salary, Title, Yrs_worked, Email) VALUES(?, ?, ?, ?, ?, ?)",
+           [4, 'Sarah', 13500, 'Newhire', 1, 'sarahfuss@gmail.com' ])
+
+db.execute("INSERT INTO dive_trips(DTID, Location, Max_depth, Cert_level_req, EID, Cost_per_diver) VALUES(?,?,?,?,?,?)",
+           [0, "Gulf of Mexico",300,0,2,150.25])
+db.execute("INSERT INTO dive_trips(DTID, Location, Max_depth, Cert_level_req, EID, Cost_per_diver) VALUES(?,?,?,?,?,?)",
+           [1, "Gulf of Mexico",200,3,3,150.25])
+db.execute("INSERT INTO dive_trips(DTID, Location, Max_depth, Cert_level_req, EID, Cost_per_diver) VALUES(?,?,?,?,?,?)",
+           [5, "Lake Apopka",100,1,2,45])
+db.execute("INSERT INTO dive_trips(DTID, Location, Max_depth, Cert_level_req, EID, Cost_per_diver) VALUES(?,?,?,?,?,?)",
+           [2, "Tampa Bay",170,4,1,35])
+
+puts "Data inserted into table"
 
 
